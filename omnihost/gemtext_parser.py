8d569877661d42edc9eedf412624755721004bf2
Brett Gleason <brettmgleason@gmail.com>
2022-07-30 14:38:11 -0500

    fix linting/formatting/type hint errors

--- omnihost/gemtext_parser.py
+++ omnihost/gemtext_parser.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum, auto
 from os import PathLike
 
@@ -20,56 +21,63 @@ class ParseMode(Enum):
     PREFORMATTED = auto()
     EOF = auto()
 
-@dataclass
-class GemLine(frozen=True):
+
+@dataclass(frozen=True)
+class GemLine:
     line_type: LineType
     line_contents: str
 
 
 class GemtextParser:
-    def __init__():
+    def __init__(self) -> None:
         self._parse_mode = ParseMode.DEFAULT
         self._STATE_TOGGLE_SYMBOL = "```"
-    
-    def parse_file_to_gemlines(file_path: PathLike) -> list[GemLine]:
+
+    def parse_file_to_gemlines(self, file_path: PathLike) -> list[GemLine]:
         self._parse_mode = ParseMode.DEFAULT
-        gemlines = []
-        
+        gemlines: list[GemLine] = []
+
         with open(file_path, mode="r", encoding="utf-8") as f:
             while self._parse_mode != ParseMode.EOF:
                 line = f.readline()
                 if not line:
                     self._parse_mode = ParseMode.EOF
-                elif self._line_is_empty(line)
+                elif self._line_is_empty(line):
                     # blank lines in Gemtext are just for the author's convenience
                     pass
                 else:
                     if self._line_toggles_parse_mode(line):
                         self._toggle_parse_mode()
-                        if self._parse_mode == ParseMode.PREFORMATTED and len(line) > len(self._STATE_TOGGLE_SYMBOL):
-                            gemlines.append(GemLine(LineType.PREFORMATTED_ALT_TEXT), line[3:])
+                        if self._parse_mode == ParseMode.PREFORMATTED and len(line) > len(
+                            self._STATE_TOGGLE_SYMBOL
+                        ):
+                            gemlines.append(
+                                GemLine(LineType.PREFORMATTED_ALT_TEXT, line[3:])
+                            )
                         elif self._parse_mode == ParseMode.DEFAULT:
                             # proceed to the next line if this line closes the preformatted block
                             # text after the "```" will be ignored
                             pass
                     else:
                         if self._parse_mode == ParseMode.PREFORMATTED:
-                            gemlines.append(GemLine(LineType.PREFORMATTED), line)
-                        
+                            gemlines.append(GemLine(LineType.PREFORMATTED, line))
+
                         elif self._parse_mode == ParseMode.DEFAULT:
                             gemline = self._parse_default_mode_gemline(line)
                             gemlines.append(gemline)
-                        
-    def _line_is_empty(self, line):
+
+        return gemlines
+
+    def _line_is_empty(self, line: str) -> bool:
         return line == "\n"
-                        
+
     def _line_toggles_parse_mode(self, line: str) -> bool:
         if len(line) >= 3:
             if line[:3] == self._STATE_TOGGLE_SYMBOL:
                 return True
 
         return False
-            
+
     def _toggle_parse_mode(self) -> None:
         if self._parse_mode == ParseMode.DEFAULT:
             self._parse_mode = ParseMode.PREFORMATTED
@@ -78,12 +86,12 @@ class GemtextParser:
         else:
             # invalid parse mode, TODO: throw unique exception instead
             raise Exception("Attempted to toggle un-toggleable parse mode.")
-        
-    def _parse_default_mode_gemline(self, line) -> GemLine:
+
+    def _parse_default_mode_gemline(self, line: str) -> GemLine:
         """Parse a gemline in default mode. This method assumes:
-         * this line does not toggle the parse mode
-         * this line is not empty (i.e. "\n")
-         * this line is not blank (i.e. "", EOF)
+        * this line does not toggle the parse mode
+        * this line is not empty (i.e. "\n")
+        * this line is not blank (i.e. "", EOF)
         """
         line_type = LineType.UNINITIALIZED
         line_content = ""
@@ -111,9 +119,9 @@ class GemtextParser:
         elif len(line) >= 1 and line[:1] == ">":
             line_type = LineType.BLOCKQUOTE
             line_content = line[1:]
-            
+
         else:
             line_type = LineType.TEXT
             line_content = line
-            
+
         return GemLine(line_type, line_content)
